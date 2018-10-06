c25645d5b579f389d6b525ada051655a87334cf8
Gotus <stalnoe5555@mail.ru>
2018-10-06 20:19:02 +0300

    Был убран лишний код.

--- src/com/gotus/Main.java
+++ src/com/gotus/Main.java
@@ -23,47 +23,24 @@ class TestFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         getContentPane().setLayout(new BorderLayout());
-        JPanel panel = new JPanel();
-        //panel.setLayout(new GridLayout());
 
+        GameData gameData = new GameData();
 
-        panel.setFocusable(true);
-
-        panel.addKeyListener(new KeyAdapter() {
+        gameData.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-
-                int keyCode = e.getKeyCode();
-
-                switch (keyCode) {
-                    case 27:
-                        System.exit(0);
-                        break;
-                }
-                ((GameData)panel.getComponent(0)).showField();
+                gameData.showField();
             }
         });
-
-        panel.add(new GameData());
 
         JLabel label = new JLabel("Расставьте фишки соответственно цветам, стоящим над столбцом");
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setVerticalAlignment(SwingConstants.NORTH);
-        label.setAlignmentY(100f);
-
-        Squares squares = new Squares();
 
         setPreferredSize(new Dimension(800, 800));
-        getContentPane().add(panel, BorderLayout.SOUTH);
+        getContentPane().add(gameData, BorderLayout.CENTER);
         getContentPane().add(label, BorderLayout.NORTH);
-        getContentPane().add(squares, BorderLayout.CENTER);
 
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                squares.addSquare(250 + j*60, 250 + i*60, 50, 50);
-            }
-        }
         //setLocationRelativeTo(null);
         setVisible(true);
     }
